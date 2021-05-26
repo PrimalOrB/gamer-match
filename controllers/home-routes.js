0e4e3bd03669d41c420ec2ca6424106a59c22922
liaof <f.liao163@gmail.com>
2021-05-26 19:51:33 -0400

    add css

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -4,4 +4,11 @@ router.get('/', (req, res) => {
   res.render('homepage');
 });
 
+router.get('/login', (req,res) => {
+    // if (req.session.loggedin) {
+    //     res.redirect('/');
+    //     return;// return to the homepage if we are already loggined in
+    // }
+    res.render('login');
+})
 module.exports = router;
