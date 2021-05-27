919df47a58e8a524bb5ff896c620085fc001d77c
powerar <arpower@bell.net>
2021-05-26 21:14:40 -0400

    added parseGameData function to loop through objects and append steamId to them

--- public/javascript/app.js
+++ public/javascript/app.js
@@ -5,34 +5,34 @@ const fetch = require('node-fetch');
 const parseString = require('xml2js').parseString;
 
 //get the steam user's owned games and stats
-const getOwnedGames = function (steamId) {
+const getOwnedGames = function () {
   // format the Steam API url with api key and steam username
-  var apiUrl = `https://api.steampowered.com/IPlayerService/GetRecentlyPlayedGames/v0001/?key=${process.env.API_KEY}&steamid=${steamId}&format=json`;
+  let steamId = '76561198014930257';
+  var apiUrl = `https://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=${process.env.API_KEY}&steamid=${steamId}&include_appinfo=true&format=json`;
 
   //make request to the url
   fetch(apiUrl).then((response) => {
     if (response.ok) {
       //parse the list of games
       response.json().then((data) => {
-        let games = data.response.games;
-        console.log(games);
+        let playerGameData = data.response.games;
+        parseGameData(playerGameData, steamId);
       });
     }
   });
 };
 
-const getSteamId = function () {
-  var steamUsername = 'uuuuggghhhhhhhh';
-  var apiUrl = `https://steamcommunity.com/id/${steamUsername}/?xml=1`;
-
-  fetch(apiUrl)
-    .then((response) => response.text())
-    .then((data) => {
-      parseString(data, function (err, result) {
-        var steamId = result['profile']['steamID64'];
-        getOwnedGames(steamId);
-      });
+const parseGameData = function(playerGameData, steamId) {
+    //loop through the playerGameData array and grab only the relevant info + steamId
+    playerGameData.forEach(function (games) {
+        var gameData = {
+            steamId,
+            name: games.name, 
+            img: games.img_logo_url,
+            playtime: games.playtime_forever
+        }
+        console.log(gameData);
     });
-};
+}
 
-getSteamId();
+getOwnedGames();
