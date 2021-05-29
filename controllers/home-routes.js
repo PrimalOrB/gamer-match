f0b67e3eb2d022e702bd9beffe20966493366e7a
liaof <f.liao163@gmail.com>
2021-05-28 23:27:41 -0400

    can successfully render data passed from clientbound routes, peripheral components are HTML hardcoded for now

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -2,7 +2,27 @@ const router = require('express').Router();
 const { User, Game, UserGame } = require('../models');
 
 router.get('/', (req, res) => {
-  res.render('homepage');
+  res.render('homepage', {
+    id: 1,
+    appid: 70,
+    name: 'Half-Life',
+    img_icon_url: 'https://icons.iconarchive.com/icons/papirus-team/papirus-apps/256/half-life-icon.png',
+    hours_played: 80
+  });
+});
+
+router.get('/', (req, res) => {
+  Game.findAll({
+    attributes: [
+      'id',
+      'appid',
+      'name',
+      'img_icon_url'
+    ]
+  })
+    .then(dbGameData => {
+
+    })
 });
 
 router.get('/login', (req,res) => {
@@ -13,7 +33,31 @@ router.get('/login', (req,res) => {
     res.render('login');
 });
 
-router.get('/user/:id', (req, res) => {
-  User.findOne()
+router.get('/game/:id', (req, res) => {
+  Game.findOne();
 });
+
+router.get('/game', (req, res) => {
+
+  res.render('single-game', {
+    id: 1,
+    appid: 70,
+    name: 'Half-Life',
+    img_icon_url: 'https://icons.iconarchive.com/icons/papirus-team/papirus-apps/256/half-life-icon.png',
+    hours_played: 80
+  });
+});
+
+router.get('/user', (req, res) => {
+
+  res.render('single-user', {
+    id: 1,
+    username: 'UltronTest',
+    steamid: 5788410,
+    profileurl: 'https://steamcommunity.com/id/UltronTest',
+    hours_played: 64
+  });
+});
+
+
 module.exports = router;
