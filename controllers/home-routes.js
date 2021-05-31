5425319e02c0ee3e6bff6191e6914cec49a2ffd1
liaof <f.liao163@gmail.com>
2021-05-31 18:10:25 -0400

    front end: homepage userpage gamepage linked together

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,62 +1,122 @@
 const router = require('express').Router();
 const { User, Game, UserGame } = require('../models');
 
-router.get('/', (req, res) => {
-  res.render('homepage', {
-    id: 1,
-    appid: 70,
-    name: 'Half-Life',
-    img_icon_url: 'https://icons.iconarchive.com/icons/papirus-team/papirus-apps/256/half-life-icon.png',
-    hours_played: 80
-  });
-});
-
-router.get('/', (req, res) => {
+router.get('/', ( req, res ) => {
   Game.findAll({
-    attributes: [
-      'id',
-      'appid',
-      'name',
-      'img_icon_url'
-    ]
-  })
-    .then(dbGameData => {
-
-    })
+      include: [{
+                  model: User,
+                  through: UserGame,
+                  as: 'played_by'
+              }]
+      })
+      .then(dbGameData => {
+        const games = dbGameData.map(game => game.get({ plain: true }));
+  
+        res.render('homepage', {
+          games,
+          // loggedIn: req.session.loggedIn
+        });
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
 });
 
 router.get('/login', (req,res) => {
-    // if (req.session.loggedin) {
-    //     res.redirect('/');
-    //     return;// return to the homepage if we are already loggined in
-    // }
-    res.render('login');
+  // if (req.session.loggedin) {
+  //     res.redirect('/');
+  //     return;// return to the homepage if we are already loggined in
+  // }
+  res.render('login');
 });
 
-router.get('/game/:id', (req, res) => {
-  Game.findOne();
+router.get('/game/:id', (req,res) => {
+    Game.findOne({
+        include: [
+            {
+                model: User,
+                attributes: ['id','username','steamid','profileurl','avatarhash'],
+                through: UserGame,
+                as: 'played_by'
+            }
+        ],
+        where: {
+            id: req.params.id
+        }
+    } )
+    .then(dbGameData => {
+      if (!dbGameData) {
+        res.status(404).json({ message: 'No post found with this id' });
+        return;
+      }
+
+      const game = dbGameData.get({ plain: true });
+
+      res.render('single-game', {
+        game
+      });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
-router.get('/game', (req, res) => {
 
-  res.render('single-game', {
-    id: 1,
-    appid: 70,
-    name: 'Half-Life',
-    img_icon_url: 'https://icons.iconarchive.com/icons/papirus-team/papirus-apps/256/half-life-icon.png',
-    hours_played: 80
+router.get('/user/:id', (req,res) => {
+  User.findOne({
+      include: [
+          {
+            model: Game,
+            attributes: ['id', 'appid', 'name', 'img_icon_url', 'img_logo_url'],
+            through: UserGame,
+            as: 'games_played'
+          }
+      ],
+      where: {
+          id: req.params.id
+      }
+  } )
+  .then(dbUserData => {
+    if (!dbUserData) {
+      res.status(404).json({ message: 'No user found with this id' });
+      return;
+    }
+
+    const user = dbUserData.get({ plain: true });
+
+    res.render('single-user', {
+      user
+    });
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
   });
 });
 
-router.get('/user', (req, res) => {
-
-  res.render('single-user', {
-    id: 1,
-    username: 'UltronTest',
-    steamid: 5788410,
-    profileurl: 'https://steamcommunity.com/id/UltronTest',
-    hours_played: 64
-  });
+//      ***catch unspeced urls***
+router.get('*', ( req, res ) => {
+  Game.findAll({
+      include: [{
+                  model: User,
+                  through: UserGame,
+                  as: 'played_by'
+              }]
+      })
+      .then(dbGameData => {
+        const games = dbGameData.map(game => game.get({ plain: true }));
+  
+        res.render('homepage', {
+          games,
+          // loggedIn: req.session.loggedIn
+        });
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
 });
 
 
