fb628332e87d9da5d52e952a2ec81c609e95fc3e
powerar <arpower@bell.net>
2021-05-31 18:42:55 -0400

    added create user function:

--- public/javascript/app.js
+++ public/javascript/app.js
@@ -1,13 +1,13 @@
 const path = require('path');
 require('dotenv').config({ path: path.resolve(__dirname, '../../.env') });
-const { User } = require('../../models/User');
+const User  = require('../../models/User');
 const fetch = require('node-fetch');
 
 //get the steam user's owned games and stats
 const getOwnedGames = function () {
   // format the Steam API url with api key and steam username
-  let steamId = '76561198014930257';
-  var apiUrl = `https://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=${process.env.API_KEY}&steamid=${steamId}&include_appinfo=true&format=json`;
+  let steamID = '76561198014930257';
+  var apiUrl = `https://api.steampowered.com/IPlayerService/GetRecentlyPlayedGames/v0001/?key=${process.env.API_KEY}&steamid=${steamID}&count=5&format=json`;
 
   //make request to the url
   fetch(apiUrl).then((response) => {
@@ -15,23 +15,61 @@ const getOwnedGames = function () {
       //parse the list of games
       response.json().then((data) => {
         let playerGameData = data.response.games;
-        parseGameData(playerGameData, steamId);
+        console.log(playerGameData, steamID);
       });
     }
   });
 };
 
-const parseGameData = function(playerGameData, steamId) {
-    //loop through the playerGameData array and grab only the relevant info + steamId
-    playerGameData.forEach(function (games) {
-        var gameData = {
-            steamId,
-            name: games.name, 
-            img: games.img_logo_url,
-            playtime: games.playtime_forever
-        }
-        console.log(gameData);
-    });
+const createUser = function () {
+  let User = 
 }
+
+
+const postGameData = async function (playerGameData, steamID) {
+  //loop through the playerGameData array and grab only the relevant info
+  playerGameData.forEach(function (games) {
+    var game = {
+      appid: games.appid,
+      name: games.name,
+      img_icon_url: games.img_icon_url,
+      img_logo_url: games.img_logo_url,
+    };
+
+    var playedGames = {
+      user_id: steamID,
+      game_id: games.appid,
+      playtime: games.playtime_forever,
+    }
+    fetch('http://localhost:3001/api/games', {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(game)
+    }).then((response) => {
+      if (response.ok) {
+        return response.json();
+      }
+      return response.json();
+    });
+
+    fetch('http://localhost:3001/api/usergames', {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(playedGames)
+    }).then(response => {
+      if (response.ok) {
+        return response.json();
+      }
+
+      return response.json();
+    })
+  });
+};
 
 getOwnedGames();
