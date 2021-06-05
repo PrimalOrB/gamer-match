1dae61671d36bd92008b12f4beaaf4b74457723d
liaof <f.liao163@gmail.com>
2021-06-05 07:19:53 -0400

    add dashboard, add session data to template

--- server.js
+++ server.js
@@ -69,7 +69,7 @@ app.use( express.static( path.join( __dirname, 'public' ) ) );
 
 app.use(session({
    secret: 'your secret',
-   name: 'name of session id,
+   name: 'name of session id',
    resave: true,
    saveUninitialized: true}));
 
